0bbc7bbd0cad63d0e15dc59ed9282ba056b00573
roger254 <rogerbrian.mutua@gmail.com>
2019-07-02 11:52:40 +0300

    init

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 # contents of selenium_docker.py
 
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
+cap = DesiredCapabilities().FIREFOX
+cap["marionette"] = False
+driver = webdriver.Firefox(capabilities=cap, executable_path='/usr/local/bin/geckodriver')
 driver.get("http://www.qxf2.com")
 print(driver.title)
 driver.quit()
